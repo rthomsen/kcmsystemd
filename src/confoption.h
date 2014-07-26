62501071bdb10a4e225ca0e79bf434e7ca34993b
rthomsen <rthomsen6@gmail.com>
2014-07-26 21:46:02 +0200

    Bump version to 0.7.0

--- src/confoption.h
+++ src/confoption.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2013 Ragnar Thomsen <rthomsen6@gmail.com>                     *
+ * Copyright (C) 2013-2014 Ragnar Thomsen <rthomsen6@gmail.com>                *
  *                                                                             *
  * This program is free software: you can redistribute it and/or modify it     *
  * under the terms of the GNU General Public License as published by the Free  *
