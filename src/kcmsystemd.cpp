c738c1607feb8258e83eff2e02b03f02169573b6
Alexandre Detiste <alexandre.detiste@gmail.com>
2014-12-21 10:56:44 +0100

    factor out color changing

--- src/kcmsystemd.cpp
+++ src/kcmsystemd.cpp
@@ -1284,21 +1284,20 @@ void kcmsystemd::slotRefreshUnitsList()
   }
   
   // Update the text color in model
+  QColor newcolor;
   for (int row = 0; row < unitsModel->rowCount(); ++row)
   {
     if (unitsModel->data(unitsModel->index(row,1), Qt::DisplayRole) == "inactive") {
-      for (int col = 0; col < 4; ++col)
-	unitsModel->setData(unitsModel->index(row,col), QVariant(QColor(Qt::red)), Qt::ForegroundRole);
+      newcolor = Qt::red;
     } else if (unitsModel->data(unitsModel->index(row,1), Qt::DisplayRole) == "active") {
-      for (int col = 0; col < 4; ++col)
-	unitsModel->setData(unitsModel->index(row,col), QVariant(QColor(Qt::darkGreen)), Qt::ForegroundRole);
+      newcolor = Qt::darkGreen;
     } else if (unitsModel->data(unitsModel->index(row,1), Qt::DisplayRole) == "failed") {
-      for (int col = 0; col < 4; ++col)
-	unitsModel->setData(unitsModel->index(row,col), QVariant(QColor(Qt::darkRed)), Qt::ForegroundRole); 
+      newcolor = Qt::darkRed;
     } else if (unitsModel->data(unitsModel->index(row,1), Qt::DisplayRole) == "-") {
-      for (int col = 0; col < 4; ++col)
-	unitsModel->setData(unitsModel->index(row,col), QVariant(QColor(Qt::darkGray)), Qt::ForegroundRole);
-    }
+      newcolor = Qt::darkGray;
+    } else newcolor = Qt::black;
+    for (int col = 0; col < 4; ++col)
+      unitsModel->setData(unitsModel->index(row,col), QVariant(newcolor), Qt::ForegroundRole);
   }
   
   // Update unit properties for the selected unit
